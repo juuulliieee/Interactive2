3910269ef24a3ccb484209f5e64aed2d304b3b1f
juuulliieee <slee17@cca.edu>
2017-09-11 14:31:40 -0700

    dfds

---
+++ exercise-charrette/assets/script.js
@@ -0,0 +1,155 @@
+$(document).ready(function(){
+	
+	$('#button-3').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+	
+	$('#button-24').hover(function() {
+		$(".item").toggleClass('pulse');
+	});
+	
+	$('#button-28').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '1in',
+			marginTop:  '1in',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+	$('#button-30').hover(function() {
+		$(".item").toggleClass('random1a');
+	});
+
+	$('#button-31').hover(function() {
+		$(".item").toggleClass('random2a');
+
+	});
+
+	$('#button-32').hover(function() {
+		$(".item").toggleClass('random3a');
+
+	});
+
+	$('#button-33').hover(function() {
+		$(".item").toggleClass('random4a');
+
+	});
+
+	$('#button-34').hover(function() {
+		$(".item").toggleClass('random5a');
+
+	});
+
+	$('#button-35').hover(function() {
+		$(".item").toggleClass('random6a');
+
+	});
+
+	$('#button-36').hover(function() {
+		$(".item").toggleClass('random7a');
+
+	});
+
+	$('#button-37').hover(function() {
+		$(".item").toggleClass('random8a');
+
+	});
+
+	$('#button-38').hover(function() {
+		$(".item").toggleClass('random9a');
+
+	});
+
+	$('#button-39').hover(function() {
+		$(".item").toggleClass('random10a');
+
+	});
+
+	$('#button-40').hover(function() {
+		$(".item").toggleClass('random11a');
+
+	});
+
+	$('#button-41').hover(function() {
+		$(".item").toggleClass('random12a');
+
+	});
+
+
+	$('#button-42').hover(function() {
+		$(".item").toggleClass('random13a');
+
+	});
+
+	$('#button-43').hover(function() {
+		
+		$(".item").toggleClass('random14a');
+
+	});
+
+	$('#button-44').hover(function() {
+		
+		$(".item").toggleClass('random15a');
+
+	});
+
+	$('#button-45').hover(function() {
+		
+		$(".item").toggleClass('random16a');
+
+	});
+
+	$('#button-46').hover(function() {
+		
+		$(".item").toggleClass('random17a');
+
+	});
+
+	$('#button-47').hover(function() {
+		
+		$(".item").toggleClass('random18a');
+
+	});
+
+	$('#button-48').hover(function() {
+		
+		$(".item").toggleClass('random19a');
+
+	});
+
+	$('#button-49').hover(function() {
+		
+		$(".item").toggleClass('random20a');
+
+	});
+
+	$('#button-50').hover(function() {
+		
+		$(".item").toggleClass('random21a');
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
